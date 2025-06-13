fa20dd4906d8dd067d0e43ded6e0a48762462c19
Kyse7535 <ktevot@gmail.com>
2025-06-13 05:54:20 +0000

    wip

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
 }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+        .then((reg) => console.log("service worker registred", reg))
+        .catch((err) => console.log("service worker not registered", err))
+}
